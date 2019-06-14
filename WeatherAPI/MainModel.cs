fa963253ac5eb1901d49a4b3094cd42f0bdced60
Justin <Tracyjn@mail.uc.edu>
2019-06-14 01:59:57 -0400

    Created MainModel class

---
+++ WeatherAPI/MainModel.cs
@@ -0,0 +1,11 @@
+namespace WeatherAPI
+{
+    public class MainModel
+    {
+        public double Temp { get; set; }
+        public double Pressure { get; set; }
+        public double Humidity { get; set; }
+        public double Min_Temp { get; set; }
+        public double Max_Temp { get; set; }
+    }
+}
